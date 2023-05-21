01d5c8d328d175119f9891826500e2e63d9df179
Jean-Dv <mrjunior127@gmail.com>
2023-05-21 18:46:17 -0500

    test: :test_tube: add domain for Courses

--- tests/Contexts/Mooc/Courses/domain/CourseMother.ts
+++ tests/Contexts/Mooc/Courses/domain/CourseMother.ts
@@ -1,7 +1,11 @@
-import { Course } from "../../../../../src/Contexts/Mooc/Courses/domain/Course"
+import { type CreateCourseCommand } from "./../../../../../src/Contexts/Mooc/Courses/domain/CreateCourseCommand"
 import { type CourseDuration } from "../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration"
 import { type CourseId } from "../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId"
 import { type CourseName } from "../../../../../src/Contexts/Mooc/Courses/domain/CourseName"
+import { Course } from "../../../../../src/Contexts/Mooc/Courses/domain/Course"
+import { CourseIdMother } from "../../Shared/domain/Courses/CourseIdMother"
+import { CourseNameMother } from "./CourseNameMother"
+import { CourseDurationMother } from "./CourseDurationMother"
 export class CourseMother {
   static create(
     id: CourseId,
@@ -9,5 +13,21 @@ export class CourseMother {
     duration: CourseDuration
   ): Course {
     return new Course(id, name, duration)
+  }
+
+  static from(command: CreateCourseCommand): Course {
+    return this.create(
+      CourseIdMother.create(command.id),
+      CourseNameMother.create(command.name),
+      CourseDurationMother.create(command.duration)
+    )
+  }
+
+  static random(): Course {
+    return this.create(
+      CourseIdMother.random(),
+      CourseNameMother.random(),
+      CourseDurationMother.random()
+    )
   }
 }
---
+++ tests/Contexts/Shared/domain/WordMother.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { MotherCreator } from "./MotherCreator"
+
+export class WordMother {
+  static random({
+    minLength = 1,
+    maxLength,
+  }: {
+    minLength?: number
+    maxLength: number
+  }): string {
+    return (
+      MotherCreator.random().lorem.word(
+        Math.floor(Math.random() * (maxLength - minLength)) + minLength
+      ) || "word"
+    )
+  }
+}
