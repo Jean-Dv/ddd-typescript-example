8e2b53875bb8098a7a67d5b98dc25c8e70f835cd
Jean-Dv <mrjunior127@gmail.com>
2023-05-21 19:53:32 -0500

    refactor: :recycle: Refactor Course

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,15 +1,56 @@
-import { type CourseId } from "../../Shared/domain/Courses/CourseId"
-import { type CourseName } from "./CourseName"
-import { type CourseDuration } from "./CourseDuration"
+import { CourseId } from "./../../Shared/domain/Courses/CourseId"
+import { CourseDuration } from "./CourseDuration"
+import { CourseName } from "./CourseName"
+import { CourseCreatedDomainEvent } from "./CourseCreatedDomainEvent"
+import { AggregateRoot } from "../../../Shared/domain/AggregateRoot"
 
-export class Course {
+export class Course extends AggregateRoot {
   readonly id: CourseId
   readonly name: CourseName
   readonly duration: CourseDuration
 
   constructor(id: CourseId, name: CourseName, duration: CourseDuration) {
+    super()
     this.id = id
     this.name = name
     this.duration = duration
+  }
+
+  static create(
+    id: CourseId,
+    name: CourseName,
+    duration: CourseDuration
+  ): Course {
+    const course = new Course(id, name, duration)
+
+    course.record(
+      new CourseCreatedDomainEvent({
+        aggregateId: course.id.value,
+        name: course.name.value,
+        duration: course.duration.value,
+      })
+    )
+
+    return course
+  }
+
+  static fromPrimitives(plainData: {
+    id: string
+    name: string
+    duration: string
+  }): Course {
+    return new Course(
+      new CourseId(plainData.id),
+      new CourseName(plainData.name),
+      new CourseDuration(plainData.duration)
+    )
+  }
+
+  toPrimitives(): any {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      duration: this.duration.value,
+    }
   }
 }
