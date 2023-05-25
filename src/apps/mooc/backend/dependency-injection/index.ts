a1ad5c843fa91cb87e703864e872a1ae8336e617
Jean-Dv <mrjunior127@gmail.com>
2023-05-25 17:58:40 -0500

    fix: :arrow_down: downgrade node-dependency-injection
    Rason: The await not function in the server

--- src/apps/mooc/backend/dependency-injection/index.ts
+++ src/apps/mooc/backend/dependency-injection/index.ts
@@ -4,10 +4,6 @@ const container = new ContainerBuilder()
 const loader = new YamlFileLoader(container)
 const env = process.env.NODE_ENV ?? "dev"
 
-export const load = async (): Promise<void> => {
-  // eslint-disable-next-line n/no-path-concat
-  await loader.load(`${__dirname}/application_${env}.yml`)
-  await container.compile()
-}
-
+// eslint-disable-next-line n/no-path-concat
+loader.load(`${__dirname}/application_${env}.yml`)
 export default container
